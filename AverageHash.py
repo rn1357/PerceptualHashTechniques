24e33120a4d66e92afc23c866275d21713d4a006
rn1357 <113860673+rn1357@users.noreply.github.com>
2023-03-27 15:44:08 -0400

    Hash techniques
    Computed hamming distances for minutiae based fingerprint images using 4 difference hash techniques

---
+++ AverageHash.py
@@ -0,0 +1,29 @@
+# Import dependencies
+from PIL import Image
+import imagehash
+import numpy as np
+import glob
+import sys
+
+input_path1 = r'/Users/rakshithanagendrappa/Desktop/NormalExtracted/*.jpg'
+out_loc = '/Users/rakshithanagendrappa/Desktop/AverageHashHammingDistances.txt'
+
+original_stdout = sys.stdout
+
+image_paths1 = list(glob.glob(input_path1))
+for i in image_paths1:
+    l = i.split('/')
+    fileName = l[-1]
+    input_path2 = r'/Users/rakshithanagendrappa/Desktop/DistortedExtracted/'
+    input_path2 = input_path2 + fileName
+
+    ahash1 = imagehash.average_hash(Image.open(i))
+    ahash2 = imagehash.average_hash(Image.open(input_path2))
+    with open(out_loc, 'a') as f:
+        sys.stdout = f 
+        print(ahash1-ahash2)    #Computing hamming distance
+
+sys.stdout = original_stdout
+    
+
+
---
+++ DWT-basedHash.py
@@ -0,0 +1,26 @@
+# Import dependencies
+from PIL import Image
+import imagehash
+import numpy as np
+import glob
+import sys
+
+input_path1 = r'/Users/rakshithanagendrappa/Desktop/NormalExtracted/*.jpg'
+out_loc = '/Users/rakshithanagendrappa/Desktop/DWT-basedHashHammingDistances.txt'
+
+original_stdout = sys.stdout
+
+image_paths1 = list(glob.glob(input_path1))
+for i in image_paths1:
+    l = i.split('/')
+    fileName = l[-1]
+    input_path2 = r'/Users/rakshithanagendrappa/Desktop/DistortedExtracted/'
+    input_path2 = input_path2 + fileName
+
+    whash1 = imagehash.whash(Image.open(i))
+    whash2 = imagehash.whash(Image.open(input_path2))
+    with open(out_loc, 'a') as f:
+        sys.stdout = f 
+        print(whash1-whash2)    #Computing hamming distance
+
+sys.stdout = original_stdout
