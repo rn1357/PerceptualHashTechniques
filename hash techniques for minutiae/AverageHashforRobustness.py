fef1e6ef7d0f2c8d88f1de49d39a9c5def7bca70
rn1357 <113860673+rn1357@users.noreply.github.com>
2023-04-14 21:11:44 -0400

    Add files via upload
    This commit has py code for robustness and discrimination property and outputs for the same for minutiae type comparison.

---
+++ hash techniques for minutiae/AverageHashforRobustness.py
@@ -0,0 +1,46 @@
+# Import dependencies
+from PIL import Image
+import imagehash
+import glob
+import csv
+
+#input and output file paths
+input_path1 = r'/Users/rakshithanagendrappa/Desktop/NormalExtracted/*.jpg'
+out_loc = '/Users/rakshithanagendrappa/Desktop/MinutiaeRobustness/AverageHashHammingDistances.txt'
+
+#Lists to store the values
+fingerPrintWithHD = []
+fingerPrintWithHDList = []  #list of lists
+header = ['F_id', 'hashtech']
+image_paths1 = list(glob.glob(input_path1))
+for i in image_paths1:
+    l = i.split('/')
+    fileName = l[-1]
+    input_path2 = r'/Users/rakshithanagendrappa/Desktop/DistortedExtracted/'
+    input_path2 = input_path2 + fileName
+
+    #getting filename without extension
+    fileName_without_ext = fileName[:fileName.rindex('.')]
+
+    #putting the filename into the list
+    fingerPrintWithHD.append(fileName_without_ext)
+    
+    #computing hash values using average hash
+    ahash1 = imagehash.average_hash(Image.open(i))
+    ahash2 = imagehash.average_hash(Image.open(input_path2))
+
+    #putting the hamming distance into the list
+    fingerPrintWithHD.append(ahash1-ahash2)
+
+    #appending list to form list of lists
+    fingerPrintWithHDList.append(fingerPrintWithHD)
+    fingerPrintWithHD = []
+
+#writing into csv file   
+with open(out_loc, 'w', newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(fingerPrintWithHDList)
+            
+
+
