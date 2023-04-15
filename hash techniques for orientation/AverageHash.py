5bec7729db6b636fe6b7a168cd2a0fbd7e0c36b8
rn1357 <113860673+rn1357@users.noreply.github.com>
2023-04-14 21:22:45 -0400

    Add files via upload
    This commit has py code for robustness and discrimination property along with outputs for orientation map type of comparison.

---
+++ hash techniques for orientation/AverageHash.py
@@ -0,0 +1,46 @@
+# Import dependencies
+from PIL import Image
+import imagehash
+import glob
+import csv
+
+#input and output file paths
+input_path1 = r'/Users/rakshithanagendrappa/Desktop/NormalOrientationMaps/*.jpg'
+out_loc = '/Users/rakshithanagendrappa/Desktop/DPo/AverageHashHammingDistances.txt'
+
+#Lists to store the values
+fingerPrintWithHD = []
+fingerPrintWithHDList = []  #list of lists
+header = ['fingerprints']
+
+image_paths1 = list(glob.glob(input_path1))
+for i in image_paths1:
+    #getting filename without extension
+    l = i.split('/')
+    fileName = l[-1]
+    fileName_without_ext = fileName[:fileName.rindex('.')]
+
+    #putting the filename into the list
+    fingerPrintWithHD.append(fileName_without_ext)
+    header.append(fileName_without_ext)
+    for j in image_paths1:
+        #computing hash values using average hash
+        ahash1 = imagehash.average_hash(Image.open(i))
+        ahash2 = imagehash.average_hash(Image.open(j))
+
+        #putting the hamming distance into the list
+        fingerPrintWithHD.append(ahash1-ahash2)
+        
+        #appending list to form list of lists
+    fingerPrintWithHDList.append(fingerPrintWithHD)
+    fingerPrintWithHD = []
+
+#writing into csv file   
+with open(out_loc, 'w', newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(fingerPrintWithHDList)
+
+   
+
+
